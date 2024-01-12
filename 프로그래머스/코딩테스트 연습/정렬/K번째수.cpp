e0cfee8728671ebe70f2483ca0f3056d81fe1873
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-01-12 15:05:34 +0900

    Create K번째수.cpp

---
+++ 프로그래머스/코딩테스트 연습/정렬/K번째수.cpp
@@ -0,0 +1,19 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=cpp
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for(int i=0; i<commands.size(); i++){
+        vector<int> arr;
+        for(int j= commands[i][0]-1; j<commands[i][1]; j++){
+            arr.push_back(array[j]);
+        }
+        sort(arr.begin(), arr.end());
+        answer.push_back(arr[commands[i][2]-1]);
+    }
+    return answer;
+}
