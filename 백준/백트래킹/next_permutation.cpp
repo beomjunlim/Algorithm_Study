4901dd4cb6ca700abfe910d1f6d14c742f2346c8
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-02-21 22:12:15 +0900

    Update next_permutation.cpp

--- 백준/백트래킹/next_permutation.cpp
+++ 백준/백트래킹/next_permutation.cpp
@@ -12,9 +12,18 @@ int main(){
     cout.tie(NULL);
 
     int arr[3] = {1,2,3};
+    int tmp[3] = {0,0,1);
     do{
         for(int i=0; i<3; i++){
             cout<<arr[i]<<" ";
+        }
+        cout<<'\n';
+    } while(next_permutation(arr, arr+3));
+
+    do{
+        for(int i=0; i<3; i++){
+            if(tmp[i]==0)
+                cout<<arr[i]<<" ";
         }
         cout<<'\n';
     } while(next_permutation(arr, arr+3));
