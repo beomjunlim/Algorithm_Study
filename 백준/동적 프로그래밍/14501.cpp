72a58da40bd1980b3c58490fb3ed835633dc1bfe
임준범 <leamjunbeom@naver.com>
2024-04-07 13:49:32 +0900

    14501.cpp

--- 백준/동적 프로그래밍/14501.cpp
+++ 백준/동적 프로그래밍/14501.cpp
@@ -1,40 +1,38 @@
-#include<iostream>
-#include<algorithm>
-#include<cmath>
-#include<vector>
-#include<cstring>
-#include<queue>
-#include<stack>
-#include<climits>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-#define MAX 1500000
+int day[16];
+int pay[16];
+int ans[16];
 
-int T[MAX+1];
-int P[MAX+1];
-int dp[MAX+1];
-
-int main(){
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
+
     int n;
-    cin>>n;
+    int answer = 0;
+    cin >> n;
 
-    for(int i=0; i<n; i++){
-        cin>>T[i]>>P[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> day[i] >> pay[i];
     }
 
-    dp[0] = 0;
-
-    int max_pay = 0;
-
-    for(int i=0; i<=n; i++){
-        dp[i] = max(max_pay, dp[i]);
-        if(T[i]+i<=n)
-            dp[T[i]+i] = max(dp[i+T[i]], P[i] + dp[i]);
-        max_pay = max(max_pay, dp[i]);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (i + day[i] <= n)
+        {
+            ans[i] = max(ans[i + 1], ans[i + day[i]] + pay[i]);
+        }
+        else
+        {
+            ans[i] = ans[i + 1];
+        }
+        answer = max(answer, ans[i]);
     }
-    cout<<max_pay;
+
+    cout << answer;
 }
