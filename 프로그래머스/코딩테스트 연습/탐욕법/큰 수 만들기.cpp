d8c2e15cac88e35218202f73634def0c218e25e9
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-10-02 12:14:29 +0900

    Update 큰 수 만들기.cpp

--- 프로그래머스/코딩테스트 연습/탐욕법/큰 수 만들기.cpp
+++ 프로그래머스/코딩테스트 연습/탐욕법/큰 수 만들기.cpp
@@ -2,33 +2,33 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <unordered_map>
 using namespace std;
-
 
 
 string solution(string number, int k) {
     string answer = "";
-    int sw = 1;
     
-    while(sw){
-        for(int i=1; i<number.length(); i++){
-            if(number[i-1]<number[i] && k>0){
-                number.erase(i-1, 1);
-                k--;
-                if(k==0)
-                    sw = 0;
+    int idx = 1;
+    while(1){
+        if(number[idx-1]<number[idx]&&k>0){
+            number.erase(idx-1,1);
+            k--;
+            if(idx!=1)
+                idx--;
+            if(k==0)
                 break;
-            }
-            if(i == number.length()-1)
-                sw = 0;
+        }else{
+            idx++;
+            if(idx==number.length())
+                break;
         }
     }
     
-    while(k--){
-        number.erase(number.length()-1, 1);
+    while(k>0){
+        number.erase(number.length()-1,1);
+        k--;
     }
+    
     answer = number;
     return answer;
 }
-
